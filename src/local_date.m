77561c6eb6daef1f160cda2c6148164acc7f6575
Julien Fischer <juliensf@gmail.com>
2019-03-14 21:42:57 +1100

    Wrap more of LocalDate.
    src/local_date.m:
       As above.

--- src/local_date.m
+++ src/local_date.m
@@ -17,7 +17,25 @@
 
 :- type local_date.
 
+:- func get_day_of_month(local_date) = int.
+
+:- func get_day_of_year(local_date) = int.
+
+:- func get_month_value(local_date) = int.
+
+:- func get_year(local_date) = int.
+
+:- pred is_before(local_date::in, local_date::in) is semidet.
+
+:- pred is_after(local_date::in, local_date::in) is semidet.
+
+:- pred is_equal(local_date::in, local_date::in) is semidet.
+
+:- pred is_leap_year(local_date::in) is semidet.
+
 :- pred now(local_date::out, io::di, io::uo) is det.
+
+:- pred parse(string::in, local_date::out) is semidet.
 
 :- func plus_days(local_date, int64) = local_date.
 
@@ -50,10 +68,97 @@
 %---------------------------------------------------------------------------%
 
 :- pragma foreign_proc("Java",
+    get_day_of_month(D::in) = (N::out),
+    [will_not_call_mercury, promise_pure, thread_safe],
+"
+    N = D.getDayOfMonth();
+").
+
+%---------------------------------------------------------------------------%
+
+:- pragma foreign_proc("Java",
+    get_day_of_year(D::in) = (N::out),
+    [will_not_call_mercury, promise_pure, thread_safe],
+"
+    N = D.getDayOfYear();
+").
+
+%---------------------------------------------------------------------------%
+
+:- pragma foreign_proc("Java",
+    get_month_value(D::in) = (N::out),
+    [will_not_call_mercury, promise_pure, thread_safe],
+"
+    N = D.getMonthValue();
+").
+
+%---------------------------------------------------------------------------%
+
+:- pragma foreign_proc("Java",
+    get_year(D::in) = (Y::out),
+    [will_not_call_mercury, promise_pure, thread_safe],
+"
+    Y = D.getYear();
+").
+
+%---------------------------------------------------------------------------%
+
+:- pragma foreign_proc("Java",
+    is_before(A::in, B::in),
+    [will_not_call_mercury, promise_pure, thread_safe],
+"
+    SUCCESS_INDICATOR = A.isBefore(B);
+").
+
+%---------------------------------------------------------------------------%
+
+:- pragma foreign_proc("Java",
+    is_after(A::in, B::in),
+    [will_not_call_mercury, promise_pure, thread_safe],
+"
+    SUCCESS_INDICATOR = A.isAfter(B);
+").
+
+%---------------------------------------------------------------------------%
+
+:- pragma foreign_proc("Java",
+    is_equal(A::in, B::in),
+    [will_not_call_mercury, promise_pure, thread_safe],
+"
+    SUCCESS_INDICATOR = A.isEqual(B);
+").
+
+%---------------------------------------------------------------------------%
+
+:- pragma foreign_proc("Java",
+    is_leap_year(A::in),
+    [will_not_call_mercury, promise_pure, thread_safe],
+"
+    SUCCESS_INDICATOR = A.isLeapYear();
+").
+
+%---------------------------------------------------------------------------%
+
+:- pragma foreign_proc("Java",
     now(D::out, _IO0::di, _IO::uo),
     [will_not_call_mercury, promise_pure, thread_safe],
 "
     D = java.time.LocalDate.now();
+").
+
+%---------------------------------------------------------------------------%
+
+:- pragma foreign_proc("Java",
+    parse(S::in, D::out),
+    [will_not_call_mercury, promise_pure, thread_safe],
+"
+    try {
+        D = java.time.LocalDate.parse(S);
+        SUCCESS_INDICATOR = true;
+    } catch (java.time.format.DateTimeParseException e) {
+        D = null;
+        SUCCESS_INDICATOR = false;
+    }
 ").
 
 %---------------------------------------------------------------------------%
