c47a2498ad0cb1dfd9ef301afb41509f40bfba79
Julien Fischer <juliensf@gmail.com>
2019-05-18 10:02:51 +0000

    Wrap read() for Reader and BufferedReader.
    src/buffered_reader.m:
    src/reader.m:
       As above.

--- src/buffered_reader.m
+++ src/buffered_reader.m
@@ -14,14 +14,27 @@
 :- interface.
 
 :- import_module jio.reader.
+:- import_module jlang.
+:- import_module jlang.throwable.
 
+:- import_module char.
 :- import_module io.
+:- import_module stream.
+
+%---------------------------------------------------------------------------%
 
 :- typeclass buffered_reader(T) <= reader(T) where [].
 
 :- type buffered_reader.
 :- instance buffered_reader(buffered_reader).
 :- instance reader(buffered_reader).
+
+:- pred close(R::in, io::di, io::uo) is det <= buffered_reader(R).
+
+:- pred read(R::in, stream.result(char, throwable)::out,
+    io::di, io::uo) is det <= buffered_reader(R).
+
+:- pred ready(R::in, io::ui) is semidet <= buffered_reader(R).
 
 %---------------------------------------------------------------------------%
 %---------------------------------------------------------------------------%
@@ -32,6 +45,17 @@
 
 :- instance buffered_reader(buffered_reader) where [].
 :- instance reader(buffered_reader) where [].
+
+%---------------------------------------------------------------------------%
+
+close(R, !IO) :-
+    reader.close(R, !IO).
+
+read(R, Result, !IO) :-
+    reader.read(R, Result, !IO).
+
+ready(R, IO) :-
+    reader.ready(R, IO).
 
 %---------------------------------------------------------------------------%
 :- end_module jio.buffered_reader.
--- src/reader.m
+++ src/reader.m
@@ -13,7 +13,14 @@
 :- module jio.reader.
 :- interface.
 
+:- import_module jlang.
+:- import_module jlang.throwable.
+
+:- import_module char.
 :- import_module io.
+:- import_module stream.
+
+%---------------------------------------------------------------------------%
 
 :- typeclass reader(T) where [].
 
@@ -26,9 +33,10 @@
 
 %:- pred mark_supported(R::in, io::ui) is semidet <= reader(R).
 
-%:- pred read(R::in, io.result(char)::out, io::di, io::uo) is det <= reader(R).
+:- pred read(R::in, stream.result(char, throwable)::out,
+    io::di, io::uo) is det <= reader(R).
 
-%:- pred is_ready(R::in, io::ui) is semidet.
+:- pred ready(R::in, io::ui) is semidet <= reader(R).
 
 %:- pred reset(R::in, io::di, io::uo) is det <= reader(R).
 
@@ -73,6 +81,85 @@ close(R, !IO) :-
         Error = null;
     } catch (java.io.IOException e) {
         IsOk = bool.NO;
+        Error = e;
+    }
+").
+
+%---------------------------------------------------------------------------%
+
+read(R, Result, !IO) :-
+    do_read(R, CharCode, IsOk, Error, !IO),
+    (
+        IsOk = yes,
+        ( if CharCode = -1 then
+            Result = eof
+        else
+            Char = char.det_from_int(CharCode),
+            Result = ok(Char)
+        )
+    ;
+        IsOk = no,
+        Result = error(Error)
+    ).
+
+:- pred do_read(R::in, int::out, bool::out, throwable::out,
+    io::di, io::uo) is det.
+:- pragma foreign_proc("Java",
+    do_read(R::in, CharCode::out, IsOk::out, Error::out, _IO0::di, _IO::uo),
+    [will_not_call_mercury, promise_pure, thread_safe],
+"
+    try {
+        int c1 = ((java.io.Reader) R).read();
+        if (c1 == -1 || !java.lang.Character.isHighSurrogate((char) c1)) {
+            IsOk = bool.YES;
+            Error = null;
+            CharCode = c1;
+        } else {
+            int c2 = ((java.io.Reader) R).read();
+            if (c2 != -1 && !java.lang.Character.isLowSurrogate((char) c2)) {
+                CharCode = 0xfffd;
+            } else {
+                CharCode = Character.toCodePoint((char) c1, (char) c2);
+            }
+            IsOk = bool.YES;
+            Error = null;
+        }
+    } catch (java.io.IOException e) {
+        CharCode = 0;
+        IsOk = bool.NO;
+        Error = e;
+    }
+").
+
+%---------------------------------------------------------------------------%
+
+ready(R, IO) :-
+    do_ready(R, IsReady, IsOk, Error, IO),
+    (
+        IsOk = yes,
+        (
+            IsReady = no,
+            false
+        ;
+            IsReady = yes
+        )
+    ;
+        IsOk = no,
+        throw(java_exception(Error))
+    ).
+
+:- pred do_ready(R::in, bool::out, bool::out, throwable::out, io::ui) is det.
+:- pragma foreign_proc("Java",
+    do_ready(R::in, IsReady::out, IsOk::out, Error::out, _IO::ui),
+    [will_not_call_mercury, promise_pure, thread_safe],
+"
+    try {
+        IsReady = ((java.io.Reader) R).ready() ? bool.YES : bool.NO;
+        IsOk = bool.YES;
+        Error = null;
+    } catch (java.io.IOException e) {
+        IsOk = bool.NO;
+        IsReady = bool.NO;
         Error = e;
     }
 ").
